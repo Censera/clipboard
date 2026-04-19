3369cccc7de9411e911cf1a0c4e2c7d1982dbaee
Censera <Censera@proton.me>
2026-04-19 01:55:00 +0100

    init

---
+++ clipboard.nix
@@ -0,0 +1,26 @@
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "clipboard";
+  version = "1.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [ 
+    pkgs.gcc 
+    pkgs.pkg-config 
+  ];
+
+  buildInputs = [
+    pkgs.xorg.libxcb
+  ];
+
+  buildPhase = ''
+    gcc main.c clipboard_x11.c $(pkg-config --cflags --libs xcb) -o clipboard
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp clipboard $out/bin/
+  '';
+}
---
+++ main.c
@@ -0,0 +1,50 @@
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <err.h>
+# include "libclipboard.h"
+
+int	free_to_copy( const char *src )
+{
+	if ( 0 == fork() )
+	{	
+		clipboard_c *cb = clipboard_new(NULL);
+		if ( NULL == cb )
+		{
+			errx(1, "Failed to open clipboard!");
+		}
+		clipboard_set_text_ex( cb, src, -1, LCB_CLIPBOARD );
+
+		usleep(100000);
+
+		clipboard_free(cb);
+		exit(0);
+	}
+	return (0);
+}
+
+int	main( int argc, char** argv )
+{
+	const char *src;
+
+	if ( 1 < argc && argv[1] != NULL )
+	{
+		src = argv[1];
+	}
+	else
+	{
+		char buf[32000];
+		size_t read = fread(buf, 1, sizeof(buf) - 1, stdin);
+
+		if (0 == read)
+		{
+			errx(1, "No source to copy from");
+		}
+
+		buf[read];
+		src = buf;
+	}
+
+	int ret = free_to_copy(src);
+	return ret;
+}
